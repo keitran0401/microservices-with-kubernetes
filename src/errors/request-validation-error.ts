47c88fe2f020c9d4008d861a87d9ba75c245eb77
Nghia Tran <thnghia0401@gmail.com>
2023-02-13 11:39:21 +0700

    first commit

---
+++ src/errors/request-validation-error.ts
@@ -0,0 +1,17 @@
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+  errors: ValidationError[];
+
+  constructor(errors: ValidationError[]) {
+    super('Invalid request parameters');
+    this.errors = errors;
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((err) => ({ message: err.msg, field: err.param }));
+  }
+}
